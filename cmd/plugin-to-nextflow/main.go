03796ad7d5592a91f799d3295536f4500ed96932
Toan Phung <toan.phung@proteo.info>
2026-02-14 13:52:26 +0000

    update plugin-to-slivka

--- cmd/plugin-to-nextflow/main.go
+++ cmd/plugin-to-nextflow/main.go
@@ -16,26 +16,27 @@ func main() {
 	pluginPath := flag.String("plugin", "", "Path to plugin.yaml")
 	pluginsDir := flag.String("plugins-dir", "", "Path to directory containing multiple plugins")
 	outputDir := flag.String("output", "./nextflow-pipeline", "Output directory")
+	generateGithubAction := flag.Bool("github-action", false, "Generate GitHub Action workflow for CI/CD")
 	flag.Parse()
 
 	if *pluginPath == "" && *pluginsDir == "" {
-		fmt.Println("Usage: plugin-to-nextflow --plugin <path/to/plugin.yaml> [--output <output/dir>]")
-		fmt.Println("       plugin-to-nextflow --plugins-dir <path/to/plugins> [--output <output/dir>]")
+		fmt.Println("Usage: plugin-to-nextflow --plugin <path/to/plugin.yaml> [--output <output/dir>] [--github-action]")
+		fmt.Println("       plugin-to-nextflow --plugins-dir <path/to/plugins> [--output <output/dir>] [--github-action]")
 		os.Exit(1)
 	}
 
 	if *pluginPath != "" {
-		if err := convertPlugin(*pluginPath, *outputDir); err != nil {
+		if err := convertPlugin(*pluginPath, *outputDir, *generateGithubAction); err != nil {
 			log.Fatalf("Error converting plugin: %v", err)
 		}
 	} else if *pluginsDir != "" {
-		if err := convertPluginsBatch(*pluginsDir, *outputDir); err != nil {
+		if err := convertPluginsBatch(*pluginsDir, *outputDir, *generateGithubAction); err != nil {
 			log.Fatalf("Error converting plugins batch: %v", err)
 		}
 	}
 }
 
-func convertPluginsBatch(dir, outDir string) error {
+func convertPluginsBatch(dir, outDir string, generateGithubAction bool) error {
 	var pluginFiles []string
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -57,7 +58,7 @@ func convertPluginsBatch(dir, outDir string) error {
 	fmt.Printf("Found %d plugins for batch conversion\n", len(pluginFiles))
 
 	for _, path := range pluginFiles {
-		if err := convertPlugin(path, outDir); err != nil {
+		if err := convertPlugin(path, outDir, generateGithubAction); err != nil {
 			fmt.Printf("[WARNING] Failed to convert plugin at %s: %v\n", path, err)
 		}
 	}
@@ -65,7 +66,7 @@ func convertPluginsBatch(dir, outDir string) error {
 	return nil
 }
 
-func convertPlugin(path, outDir string) error {
+func convertPlugin(path, outDir string, generateGithubAction bool) error {
 	definition, err := parser.ParsePlugin(path)
 	if err != nil {
 		return err
@@ -73,13 +74,11 @@ func convertPlugin(path, outDir string) error {
 
 	fmt.Printf("Converting plugin: %s (%s)\n", definition.Plugin.Name, definition.Plugin.ID)
 
-	// Ensure output directory exists
 	moduleDir := filepath.Join(outDir, "modules", "local", definition.Plugin.ID)
 	if err := os.MkdirAll(moduleDir, 0755); err != nil {
 		return err
 	}
 
-	// Generate Process
 	procTmpl, _ := templates.GetTemplate("process.nf.tmpl")
 	procContent, err := generator.GenerateProcess(definition, procTmpl)
 	if err != nil {
@@ -89,7 +88,6 @@ func convertPlugin(path, outDir string) error {
 		return err
 	}
 
-	// Generate Workflow (Main NF)
 	wfTmpl, _ := templates.GetTemplate("main.nf.tmpl")
 	wfContent, err := generator.GenerateWorkflow(definition, wfTmpl)
 	if err != nil {
@@ -99,7 +97,6 @@ func convertPlugin(path, outDir string) error {
 		return err
 	}
 
-	// Generate Config
 	cfgTmpl, _ := templates.GetTemplate("nextflow.config.tmpl")
 	cfgContent, err := generator.GenerateConfig(definition, cfgTmpl)
 	if err != nil {
@@ -109,18 +106,15 @@ func convertPlugin(path, outDir string) error {
 		return err
 	}
 
-	// Generate Nextflow Schema
 	schemaContent, err := generator.GenerateSchema(definition)
 	if err == nil {
 		os.WriteFile(filepath.Join(outDir, "nextflow_schema.json"), []byte(schemaContent), 0644)
 	}
 
-	// Generate Dockerfile
 	containerDir := filepath.Join(outDir, "containers")
 	os.MkdirAll(containerDir, 0755)
 
 	if definition.Runtime.IsDockerRuntime() && definition.Runtime.Docker != nil && definition.Runtime.Docker.Dockerfile != "" {
-		// Copy custom Dockerfile
 		pluginDir := filepath.Dir(path)
 		customDockerPath := filepath.Join(pluginDir, definition.Runtime.Docker.Dockerfile)
 		dockerContent, err := os.ReadFile(customDockerPath)
@@ -135,19 +129,20 @@ func convertPlugin(path, outDir string) error {
 		}
 	}
 
-	// Generate README
 	if readmeTmpl, err := templates.GetTemplate("README.md.tmpl"); err == nil {
 		if readmeContent, err := generator.GenerateREADME(definition, readmeTmpl); err == nil {
 			os.WriteFile(filepath.Join(outDir, "README.md"), []byte(readmeContent), 0644)
 		}
 	}
 
-	// Generate GitHub Action for the plugin
-	if actionTmpl, err := templates.GetTemplate("plugin-github-action.yml.tmpl"); err == nil {
-		if actionContent, err := generator.GenerateGithubAction(definition, actionTmpl); err == nil {
-			githubDir := filepath.Join(outDir, ".github", "workflows")
-			os.MkdirAll(githubDir, 0755)
-			os.WriteFile(filepath.Join(githubDir, "nextflow-export.yml"), []byte(actionContent), 0644)
+	if generateGithubAction {
+		if actionTmpl, err := templates.GetTemplate("plugin-github-action.yml.tmpl"); err == nil {
+			if actionContent, err := generator.GenerateGithubAction(definition, actionTmpl); err == nil {
+				githubDir := filepath.Join(outDir, ".github", "workflows")
+				os.MkdirAll(githubDir, 0755)
+				os.WriteFile(filepath.Join(githubDir, "nextflow-export.yml"), []byte(actionContent), 0644)
+				fmt.Printf("Successfully generated GitHub Action: %s\n", filepath.Join(githubDir, "nextflow-export.yml"))
+			}
 		}
 	}
 
